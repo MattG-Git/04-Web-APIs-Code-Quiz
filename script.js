4672e220bd2e1fd8f352dbe3b66e00850aa2e8d8
Matt Goldhammer <Matt.Goldhammer@gmail.com>
2022-07-17 20:55:24 -0700

    updating js code

---
+++ script.js
@@ -0,0 +1,259 @@
+//selects the timer element by id
+var timeEl = document.getElementById("timer");
+//sets how much time the timer starts off with
+var timeLeft = 75;
+var score = 0;
+var allDone = true; 
+var intro = document.getElementById("intro");
+var rules = document.getElementById("instructions");
+var start = document.getElementById("startBtn");
+var begin = document.getElementById("beginning");
+var body = document.body;
+var player = "";
+
+var q1 = {
+    question:"What is an array?",
+    a1:"a light beam coming from the sun",
+    a2:"a little green monster that lives under the beds of children",
+    correct:"organizes data so that a related set of values can be easily sorted or searched",
+    a4:"an exboyfriend that smells badly"
+};
+var q2 = {
+    question:"What's the correct way to declare a variable in javaScript?",
+    correct:" var name = Jim",
+    a2:"variable name = Jim",
+    a3:"Jim = var name",
+    a4:"var name: Jim"
+};
+var q3 = {
+    question:"What is one correct syntax to write a function in javaScript?",
+    a1:"start function() {};",
+    a2:"function start {};",
+    a3:"function start ();",
+    correct:"function start() {};"
+};
+var questionArray = [q1, q2, q3];
+
+function startQuiz() {
+    function setTimer(){
+        //sets timer interval in variable
+        var timerInterval = setInterval(function() {
+            timeLeft--;
+            timeEl.textContent = timeLeft;
+    
+            if(timeLeft < 0) {
+                // stops execution of action at set interval
+                clearInterval(timerInterval);
+                // calls function to create end of quiz, display score and add initials
+                if (allDone){ endQuiz()};
+            }
+            // this 1000 makes the timer count backwards 1 second at a time or 1000 miliseconds
+        }, 1000);
+    };
+
+    begin.remove();
+
+    //this code creates an h3 element and makes it show on the screen for question 1
+    var q = document.createElement("h3");
+    body.appendChild(q);
+    q.textContent = q1["question"];
+
+    // this code creates a button element
+    var a1 = document.createElement ("button");
+    var a2 = document.createElement ("button");
+    var a3 = document.createElement ("button");
+    var a4 = document.createElement ("button");
+
+    //this code adds a click event listener so that the appropriate function is called when the buttons are clicked
+    a1.addEventListener("click", wrongAnswer)
+    a2.addEventListener("click", wrongAnswer)
+    a3.addEventListener("click", correctAnswer)
+    a4.addEventListener("click", wrongAnswer)
+
+    //this code makes the answer buttons display on screen
+    body.appendChild(a1);
+    body.appendChild(a2);
+    body.appendChild(a3);
+    body.appendChild(a4);
+    a1.textContent = q1["a1"];
+    a2.textContent = q1["a2"];
+    a3.textContent = q1["correct"];
+    a4.textContent = q1["a4"];
+
+    function correctAnswer() {
+        score ++;
+        body.removeChild(q);
+        body.removeChild(a1);
+        body.removeChild(a2);
+        body.removeChild(a3);
+        body.removeChild(a4);
+        question2();
+        console.log(score)
+    };
+    
+    function wrongAnswer() {
+        timeLeft = timeLeft - 5;
+        body.removeChild(q);
+        body.removeChild(a1);
+        body.removeChild(a2);
+        body.removeChild(a3);
+        body.removeChild(a4);
+        question2();
+        console.log(score)
+    };
+   
+    function question2() {
+        //this code creates an h3 element and makes it show on the screen for question 2
+        var q = document.createElement("h3");
+        body.appendChild(q);
+        q.textContent = q2["question"];
+
+        // this code creates a button element
+        var a1 = document.createElement ("button");
+        var a2 = document.createElement ("button");
+        var a3 = document.createElement ("button");
+        var a4 = document.createElement ("button");
+
+        //this code adds a click event listener so that the appropriate function is called when the buttons are clicked
+        a1.addEventListener("click", correctAnswer)
+        a2.addEventListener("click", wrongAnswer)
+        a3.addEventListener("click", wrongAnswer)
+        a4.addEventListener("click", wrongAnswer)
+
+        //this code makes the answer buttons display on screen
+        body.appendChild(a1);
+        body.appendChild(a2);
+        body.appendChild(a3);
+        body.appendChild(a4);
+        a1.textContent = q2["correct"];
+        a2.textContent = q2["a2"];
+        a3.textContent = q2["a3"];
+        a4.textContent = q2["a4"];
+
+    function correctAnswer() {
+        score ++;
+        body.removeChild(q);
+        body.removeChild(a1);
+        body.removeChild(a2);
+        body.removeChild(a3);
+        body.removeChild(a4);
+        question3();
+        console.log(score)
+    };
+    
+    function wrongAnswer() {
+        timeLeft = timeLeft - 5;
+        body.removeChild(q);
+        body.removeChild(a1);
+        body.removeChild(a2);
+        body.removeChild(a3);
+        body.removeChild(a4);
+        question3();
+        console.log(score)
+    };
+    }
+
+    function question3() {
+        //this code creates an h3 element and makes it show on the screen for question 1
+        var q = document.createElement("h3");
+        body.appendChild(q);
+        q.textContent = q2["question"];
+      
+        // this code creates a button element
+        var a1 = document.createElement ("button");
+        var a2 = document.createElement ("button");
+        var a3 = document.createElement ("button");
+        var a4 = document.createElement ("button");
+      
+        //this code adds a click event listener so that the appropriate function is called when the buttons are clicked
+        a1.addEventListener("click", wrongAnswer)
+        a2.addEventListener("click", wrongAnswer)
+        a3.addEventListener("click", wrongAnswer)
+        a4.addEventListener("click", correctAnswer)
+      
+        //this code makes the answer buttons display on screen
+        body.appendChild(a1);
+        body.appendChild(a2);
+        body.appendChild(a3);
+        body.appendChild(a4);
+        a1.textContent = q3["a1"];
+        a2.textContent = q3["a2"];
+        a3.textContent = q3["a3"];
+        a4.textContent = q3["correct"];
+      
+    function correctAnswer() {
+        score ++;
+        body.removeChild(q);
+        body.removeChild(a1);
+        body.removeChild(a2);
+        body.removeChild(a3);
+        body.removeChild(a4);
+        endQuiz();
+        console.log(score)
+    };
+
+    function wrongAnswer() {
+        timeLeft = timeLeft - 5;
+        body.removeChild(q);
+        body.removeChild(a1);
+        body.removeChild(a2);
+        body.removeChild(a3);
+        body.removeChild(a4);
+        endQuiz();
+        console.log(score)
+    };
+    }
+
+    function endQuiz() {
+        timeLeft = 0;   
+        allDone = false; 
+        var header = document.createElement("h3");
+        body.appendChild(header);
+        header.textContent = "All done!";
+
+        var endScore = document.createElement("p");
+        body.appendChild(endScore);
+        endScore.textContent = `Your final score is: ${score}`;
+
+        var initials = document.createElement("input");
+        body.appendChild(initials);
+        initials.setAttribute("value","");
+        localStorage.setItem("player",player);
+        console.log(initials.value);
+
+
+        var endBtn = document.createElement("button");
+        body.appendChild(endBtn);
+        endBtn.textContent = "Submit";
+        endBtn.addEventListener("click", scoreBoard);
+        
+
+        function scoreBoard() {
+            player = initials.value;
+            localStorage.setItem("player",player);
+            localStorage.setItem("score", score);
+            console.log(player);
+        
+            //remove input
+
+            //remove submit button
+
+            //change h3 text
+            
+            //create, append table and rows displaying local storage?
+
+            //create, append clear scores button
+
+            //create, append start over or go back button
+        }
+    }
+
+    setTimer();
+};
+
+
+
+intro.textContent = "CODING QUIZ CHALLENGE"
+rules.textContent = "You will have 75 seconds to complete this multiple choice quiz. If you select the wrong answer, 5 seconds will be deducted from the timer. The game is over when the timer runs out or the last question is answered. GOOD LUCK!"
+
+start.addEventListener ('click', startQuiz)
